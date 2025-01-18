8f3c61fa2affae2e3a4c44fa836a732668b3c712
Dicksen Tan <tandicksen@gmail.com>
2025-01-18 21:42:29 +0700

    Add Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,16 @@ import React from "react";
 
 const Header = ()=>{
     return (
-        <header>
-            <Image src='/chatLogo.svg' alt="Site Logo" width={32} height={32}/>
+        <header className="flex justify-between py-4 px-3 border-b md:px-8">
+            <div className="flex items-center">
+                <Image src='/chatLogo.svg' alt="Site Logo" width={32} height={32}/>
+                 <h1 className="font-bold ml-2">Chat AI</h1>
+            </div>
+            <nav>
+                <button aria-label="open menu">
+                    <Image src='/menu.svg' alt="Menu Logo" width={32} height={32}/>
+                </button>
+            </nav>
         </header>
     )
 }
