08ce49cb84962d5cb4e4a863027f34741a5fa430
Dicksen Tan <tandicksen@gmail.com>
2025-01-26 22:07:17 +0700

    Create component that need redux provider

---
+++ app/components/ProvidedComponent.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Provider } from "react-redux"
+import store from "../../redux/store"
+import Header from "./Header"
+
+
+export default function ProvidedComponent({children}: {children: React.ReactNode}){
+    return(
+        <Provider store={store}>
+            <Header/>
+            <main className="h-[calc(100vh-71px)] pt-12 pb-24 px-4 overflow-y-auto">
+                {children}
+            </main>
+        </Provider>
+    )
+}
