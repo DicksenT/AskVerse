ea1b6b41f7bdb32619d65fe95b35f035570ff95b
Dicksen Tan <tandicksen@gmail.com>
2025-01-26 22:11:59 +0700

    Add btn to toggle sidebar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import React from "react";
+import Sidebar from "./Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveMenu} from "../../redux/sidebarSlice";
+import { AppDispatch, RootState } from "../../redux/store";
 
 const Header = ()=>{
+    const appDispatch = () => useDispatch<AppDispatch>()
+    const sidebarActive = useSelector((state: RootState) => state.sidebar.activeMenu)
+    const dispacth = appDispatch()
     return (
         <header className="flex justify-between py-4 px-3 border-b md:px-8">
             <div className="flex items-center">
@@ -9,7 +16,7 @@ const Header = ()=>{
                  <h1 className="font-bold ml-2">Chat AI</h1>
             </div>
             <nav>
-                <button aria-label="open menu">
+                <button aria-label="open menu" onClick={() => dispacth(setActiveMenu())}>
                     <Image src='/menu.svg' alt="Menu Logo" width={32} height={32}/>
                 </button>
             </nav>
