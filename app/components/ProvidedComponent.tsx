4030671b2f25a4c9df85f1eb9fb83dfa0f957524
Dicksen Tan <tandicksen@gmail.com>
2025-01-26 22:16:26 +0700

    Add ProvidedComp to layout

--- app/components/ProvidedComponent.tsx
+++ app/components/ProvidedComponent.tsx
@@ -3,6 +3,7 @@
 import { Provider } from "react-redux"
 import store from "../../redux/store"
 import Header from "./Header"
+import Input from "./Input"
 
 
 export default function ProvidedComponent({children}: {children: React.ReactNode}){
@@ -12,6 +13,7 @@ export default function ProvidedComponent({children}: {children: React.ReactNode
             <main className="h-[calc(100vh-71px)] pt-12 pb-24 px-4 overflow-y-auto">
                 {children}
             </main>
+            <Input/>
         </Provider>
     )
 }
