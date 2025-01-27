4dfa31bb2fb98e9edd1bdf7ef60ac16b922947a6
Dicksen Tan <tandicksen@gmail.com>
2025-01-27 22:04:15 +0700

    Change Goose Model to Claude

---
+++ app/api/Claude.ts
@@ -0,0 +1,16 @@
+import Anthropic from '@anthropic-ai/sdk';
+
+export async function GenerateClaude(prompt: string){
+const anthropic = new Anthropic({
+  apiKey: process.env.CLAUDE_API,
+  dangerouslyAllowBrowser:true // defaults to process.env["ANTHROPIC_API_KEY"]
+});
+
+const msg = await anthropic.messages.create({
+  model: "claude-3-5-haiku-latest",
+  max_tokens: 1024,
+  messages: [{ role: "user", content: prompt }],
+});
+const text = (msg.content[0] as Anthropic.TextBlock).text;
+console.log(text);
+}
