59ef242c5e7f07c8d5a509bb5dce2bb7140a300e
Dicksen Tan <tandicksen@gmail.com>
2025-02-05 21:38:14 +0700

    REmove input comp

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import React from "react";
 import './globals.css';
-import Input from "./components/Input";
 import ProvidedComponent from "./components/ProvidedComponent";
 export const metadata: Metadata = {
   title: "Chat AI",
