fa9e8de47fa6ced199914660cc82fbc99e2bd6cc
Dicksen Tan <tandicksen@gmail.com>
2025-02-16 20:59:47 +0700

    dispatch active chat id
    if user accessed the chat through link/url

--- app/[chatId]/page.tsx
+++ app/[chatId]/page.tsx
@@ -1,11 +1,23 @@
 'use client'
 import { useParams, usePathname } from "next/navigation"
-import React from "react"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "../../redux/store"
+import { setActiveChat } from "../../redux/chatSlice"
+import { Loading } from "../components/Loading"
+import { Response } from "../components/Response"
 
 const ChatPage = () =>{
     const {chatId} = useParams()
     const pathname = usePathname()
-    console.log(pathname === '/' + chatId)
+    const dispatch = useDispatch<AppDispatch>()
+    useEffect(() =>{
+        dispatch(setActiveChat(pathname.slice(1,)))
+        return () =>{dispatch(setActiveChat(''))}
+    },[pathname])
+    const chat = useSelector((state: RootState) => state.chats.chats[pathname.slice(1,)])
+    
+    const messages = useSelector((state: RootState) => chat ?  chat.chatListsId.map(id => state.chats.messages[id]) : []) 
     return(
         <>**HELLO**</>
     )
