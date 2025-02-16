6813044ca9dbfab47b9db31264a2e94a4d232692
Dicksen Tan <tandicksen@gmail.com>
2025-02-16 21:09:54 +0700

    Link to home page and remove sidebar component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import React from "react";
-import Sidebar from "./Sidebar";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setActiveMenu} from "../../redux/sidebarSlice";
 import { AppDispatch, RootState } from "../../redux/store";
+import Link from "next/link";
 
 const Header = ()=>{
     const appDispatch = () => useDispatch<AppDispatch>()
@@ -11,16 +11,18 @@ const Header = ()=>{
     const dispacth = appDispatch()
     return (
         <header className="flex justify-between py-4 px-3 border-b md:px-8">
-            <div className="flex items-center">
-                <Image src='/chatLogo.svg' alt="Site Logo" width={32} height={32}/>
-                 <h1 className="font-bold ml-2">Chat AI</h1>
-            </div>
+            <Link href={'/'}>
+                <div className="flex items-center">
+                    <Image src='/chatLogo.svg' alt="Site Logo" width={32} height={32}/>
+                    <h1 className="font-bold ml-2">Chat AI</h1>
+                </div>
+            </Link>
             <nav>
                 <button aria-label="open menu" onClick={() => dispacth(setActiveMenu())}>
                     <Image src='/menu.svg' alt="Menu Logo" width={32} height={32}/>
                 </button>
             </nav>
-           <Sidebar/>
+          
         </header>
     )
 }
