afcab6a22d345d13507de51babe1217ce2f7128d
Dicksen Tan <tandicksen@gmail.com>
2025-02-16 21:02:17 +0700

    update width state

--- app/components/ProvidedComponent.tsx
+++ app/components/ProvidedComponent.tsx
@@ -4,9 +4,22 @@ import { Provider } from "react-redux"
 import store from "../../redux/store"
 import Header from "./Header"
 import Input from "./Input"
+import Sidebar from "./Sidebar"
+import { useEffect, useState } from "react"
 
 
 export default function ProvidedComponent({children}: {children: React.ReactNode}){
+    const [width,setWidth] = useState<number>(window.innerWidth)
+    useEffect(() =>{
+        const handleResize =()=>{
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return()=>{window.removeEventListener('resize', handleResize)}
+    },[])
+    useEffect(() =>{
+        console.log(width)
+    },[width])
     return(
         <Provider store={store}>
             <Header/>
