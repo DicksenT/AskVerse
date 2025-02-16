c2515b062c3afcf91c221cdb54169f16e4d7e823
Dicksen Tan <tandicksen@gmail.com>
2025-02-16 21:12:11 +0700

    message function
    create new function to add message,
    previously on submit function for better readbility and scaling

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -26,22 +26,75 @@ const Input = () =>{
         textRef.current.style.height = '40px'
         textRef.current.style.height = `${textRef.current.scrollHeight}px`
     }
+    const addMessageFn = async(question: string, chatId : string) =>{
+       
+        const newMessage: messageStructure = {
+            id: crypto.randomUUID(),
+            chatId: chatId,
+            question: question,
+            response: {
+                OpenAI: null,
+                Gemini: null,
+                Cohere: null,
+                DeepSeek: null,
+                Claude: null,
+                isLoading: true
+            }    
+        }
+        dispatch(addMessages(newMessage))
+        
+        /*const apiResponses = await Promise.all([
+            GenerateClaude(question),
+            GenerateCohere(question),
+            GenerateDeepSeek(question),
+            GenerateGemini(question),
+            GenerateOpenAI(question)
+        ])
+        const responseKeys = ['Claude', 'Cohere', 'DeepSeek', 'Gemini', 'OpenAI']
+        const response = apiResponses.reduce((acc, value, index)=>{
+            if(value) acc[responseKeys[index]] = value
+            return acc
+        }, {} as Partial<responseStructure>)
+
+        const finalResponse: responseStructure ={
+            ...response,
+            isLoading: false
+        }
+        dispatch(addMessagesResponse({msgId: newMessage.id, response: finalResponse}))
+        console.log(finalResponse)*/
+        
+    }
     const handleSubmit = async(e) =>{
        e.preventDefault()
-       await Promise.all([
-        GenerateClaude(text),
-        GenerateCohere(text),
-        GenerateDeepSeek(text),
-        GenerateGemini(text),
-        GenerateOpenAI(text)
-       ])
+       const question = text
+       setText('')
+       if(pathname === '/'){
+            const newChat: chatStructure = {
+                name:'New chat',
+                id: crypto.randomUUID(),
+                chatListsId: []
+            }
+            dispatch(addNewChat(newChat))
+            dispatch(setActiveChat(newChat.id))
+            setTimeout(() =>{
+                addMessageFn(question, newChat.id)
+                router.push(newChat.id)
+            },1000)
+        }
+        else{
+            addMessageFn(question, activeChat)
+        }
+
+        
     }
     return(
-        <form className="fixed bottom-6 flex items-end w-full gap-2 px-4 left-0" onSubmit={(e)=> handleSubmit(e)}>
+        <form className="fixed bottom-0 min-h-20 max-w-3xl t left-1/2 
+                transform -translate-x-1/2 pb-10 bg-white flex items-end w-full gap-2 px-4" onSubmit={(e)=> handleSubmit(e)}>
             <textarea 
             className="resize-none border rounded-lg outline-none px-4 text-xs w-[calc(100%-40px-8px)] h-10 py-3" name="" id=""
             placeholder="Ask me anything..."
-            onInput={handleInput}
+            onInput={handleInput} 
+            value={text}
             ref={textRef}></textarea>
             <button className="w-10 h-10 bg-indigo-700 p-2.5 rounded-md">
                 <Image alt="send image" src='/send-active.svg' width={20} height={20}/>
