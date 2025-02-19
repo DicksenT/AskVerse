77560b0f0b8b37d7907af8ed7ebff1457827c2fe
Dicksen Tan <tandicksen@gmail.com>
2025-02-20 00:19:45 +0700

    Create page for unknown url

---
+++ app/components/404.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+export const LostPage =() =>{
+    return(
+        <article className="relative">
+            <Image src={'404.svg'} alt="404 page" width={520} height={520}/>
+            <p className="absolute bottom-2 right-2 text-white text-xs bg-black bg-opacity-50 px-2 py-1 rounded">
+            <a href="https://storyset.com/web" target="_blank">Web illustrations by Storyset</a>
+            </p>
+        </article>
+    )
+} 
