1f77d949c954dd239350197bdb0fee4ced1df89a
Dicksen Tan <tandicksen@gmail.com>
2025-03-11 20:39:42 +0700

    Create requireAuth.ts
    middleware for auth check

---
+++ app/middleware/requireAuth.ts
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export async function requireAuth(req: NextRequest){
+    try{
+        const session = await getServerSession(authOptions)
+        if(!session || session.user){
+            return NextResponse.json({message: 'Unauthorized'}, {status: 401})
+        }
+        return {userId: session.user.id}
+    }catch(error){
+        return NextResponse.json({error: 'Authentication failed'}, {status: 401})
+    }
+}
