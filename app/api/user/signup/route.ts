5af512f0e0d72fa5e8ed43e3f777ef3ea50ef025
Dicksen Tan <tandicksen@gmail.com>
2025-03-11 20:41:35 +0700

    Update route.ts
    change status code and use auth middleware

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,12 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { addUser } from "../../../../backend/controllers/userController";
+import { requireAuth } from "../../../middleware/requireAuth";
 
 export async function POST(req: NextRequest){
     try{
+        const auth = await requireAuth(req)
+        if(auth instanceof NextResponse) return auth
         const {email, password} = await req.json()
         const newUser = await addUser(email, password)
         return NextResponse.json({message: 'User Created', data: newUser}, {status: 201})
     }catch(error){
-        return NextResponse.json({error: error.message},{status: 401})
+        return NextResponse.json({error: error.message},{status: 400})
     }
 }
