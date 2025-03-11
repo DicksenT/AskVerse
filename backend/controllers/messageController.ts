65c6c598b2c4d19271af78e1292713b2f2cd536c
Dicksen Tan <tandicksen@gmail.com>
2025-03-11 21:06:59 +0700

    Update messageController.ts
    Change data type

--- backend/controllers/messageController.ts
+++ backend/controllers/messageController.ts
@@ -1,16 +1,20 @@
+import { Types } from "mongoose";
 import { dbConnect } from "../../app/lib/dbConnect";
 import Message from "../models/messageModel";
 import Response, { IResponse } from "../models/responseModel";
+import Chat from "../models/chatModel";
 
 
-export async function addMessage(chatId: string, question: string){
+export async function addMessage(userId: Types.ObjectId,chatId: Types.ObjectId, question: string){
     await dbConnect()
     try{
-
         if(!chatId || !question){
             throw new Error('chatId and question is required')
         }
-        const newMessage = await Message.create({chatId: chatId, question: question})
+        const existChat = await Chat.findOne({userId, _id:chatId})
+        if(!existChat) throw new Error("chat is not exist")
+        const newMessage = await Message.create({chatId, question})
+        await Chat.updateOne({_id: chatId}, {$push: {messages: newMessage._id}})
         return newMessage
 
     }catch(error){
@@ -19,22 +23,22 @@ export async function addMessage(chatId: string, question: string){
     }
 }
 
-export async function addResponseToMessage(msgId: string, response: Partial<IResponse>){
+export async function addResponseToMessage(userId: Types.ObjectId, chatId: Types.ObjectId, msgId: Types.ObjectId, response: Partial<IResponse>){
     await dbConnect()
     try{
-        const message = await Message.findById(msgId)
-        if(!message){
-            throw new Error('Message do not exist')
-        }
+        const chat = await Chat.findOne({userId, _id: chatId})
+        if(!chat) throw new Error('Unauthorized')
+
+        const message = await Message.findOne({_id: msgId, chatId})
+        if(!message) throw new Error('Message do not exist')
+        
         const newResponse = await Response.create({
             messageId: msgId,
             ...response
         })
-        if(!newResponse){
-            throw new Error('failed to create respomnse')
-        }
-        message.response = newResponse._id
-        await message.save()
+        if(!newResponse) throw new Error('failed to create respomnse')
+        
+        await Message.updateOne({_id: msgId}, {response: newResponse})
         return newResponse
     }catch(error){
         console.error('failed in addResponseToMessage', error)
