3af5c57b8b6df3327d12bf1dbcd68136b60555f1
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 11:01:16 +0700

    create /message route
    route for post and fetch messages

---
+++ app/api/chat/[chatId]/message/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { addMessage, getMessages } from "../../../../../backend/controllers/messageController";
+import { requireAuth } from "../../../../lib/requireAuth";
+import { Types } from "mongoose";
+
+//post message
+export async function POST(req: NextRequest){
+    try{
+        const auth = await requireAuth(req)
+        if(auth instanceof NextResponse) return auth
+        const {chatId, question} = await req.json()
+        const newMessage = await addMessage(new Types.ObjectId(auth.userId),chatId, question)
+        return NextResponse.json({message: 'Message Created', data: newMessage}, {status: 201})
+    }catch(error){
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+}
+
+//fetch message
+export async function GET(req: NextRequest, {params}: {params: {chatId: string}}){
+    try{
+        const auth = await requireAuth(req)
+        if(auth instanceof NextResponse) return auth
+        const {chatId} = await params
+        const messages = await getMessages(new Types.ObjectId(auth.userId), new Types.ObjectId(chatId))
+        console.log(messages)
+        return NextResponse.json({messages: 'messages successfully fetched', data: messages}, {status: 200})
+    }catch(error){
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+}
