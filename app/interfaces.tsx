d25eb2ea6f61a3316f47e1def8cb0046987e9ade
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 11:17:54 +0700

    Update interfaces.tsx

--- app/interfaces.tsx
+++ app/interfaces.tsx
@@ -1,27 +1,35 @@
 export interface responseStructure{
+    _id?: string
+    msgId: string
     OpenAI: string | null
     Gemini: string | null
     Cohere: string | null
     DeepSeek: string | null
     Claude: string | null
+    personality: string
     isLoading?: boolean
+ 
 }
 
 export interface messageStructure{
-    id: string
+    _id?: string
     question:string
     chatId: string
-    response: responseStructure
+    response: responseStructure[]
 }
 
 export interface chatStructure{
+    _id?: string
+    userId: string
     name: string
-    id: string
-    chatListsId: string[]
+    messages: string[]
 }
 
 export interface chatState{
     activeChat: string
     chats: Record<string,chatStructure>
+}
+
+export interface messageState{
     messages: Record<string, messageStructure>
 }
