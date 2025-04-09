912c1421a9375ea483e01e06adabeb96c1d4c9c9
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 11:12:09 +0700

    remove from gitIgnore

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+    COHERE_API: process.env.COHERE_API,
+    CLAUDE_API: process.env.CLAUDE_API,
+    GEMINI_API: process.env.GEMINI_API,
+    DEEPSEEK_API: process.env.DEEPSEEK_API,
+    OPENAI_API: process.env.OPENAI_API,
+  }
 };
 
 export default nextConfig;
+
