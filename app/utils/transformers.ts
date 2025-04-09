6d8da042a2959b4313c0b07c730efb2397144a68
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 11:21:32 +0700

    Update transformers.ts
    add response transformer so isLoading will be false

--- app/utils/transformers.ts
+++ app/utils/transformers.ts
@@ -1,17 +1,29 @@
-import { chatStructure, messageStructure } from "../interfaces";
+import { chatStructure, messageStructure, responseStructure } from "../interfaces";
 
 export const transformMessage = (message: any): messageStructure =>({
+    //dont need to stringify _id since mongo auto-conver
     _id: message._id,
-    id: message.id,
     question: message.question,
+    //stringify the chat id since it not auto-convert
     chatId: message.chatId.toString(),
-    response: {...message.response, isLoading: true}
+    response: [{...message.response, isLoading: true}]
 })
 
 export const transformChat = (chat: any): chatStructure =>({
     _id: chat._id,
-    id: chat.id,
     userId: chat.userId.toString(),
     name: chat.name,
     messages: chat.messages.map((message:any) =>message.toString())
+})
+
+export const transformResponse = (response: any): responseStructure=>({
+    _id: response._id,
+    msgId: response.msgId.toString(),
+    isLoading: false,
+    personality: response.personality,
+    OpenAI: response.OpenAI,
+    Gemini: response.Gemini,
+    Claude: response.Claude,
+    DeepSeek: response.DeepSeek,
+    Cohere: response.Cohere,
 })
