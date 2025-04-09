2bf37eac0778bb9805c717e2097bc22553cfc6b8
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 11:04:21 +0700

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,17 @@
 import Image from "next/image";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setActiveMenu} from "../../redux/sidebarSlice";
+import { setActiveMenu} from "../../redux/stateSlice";
 import { AppDispatch, RootState } from "../../redux/store";
 import Link from "next/link";
 
 const Header = ()=>{
     const appDispatch = () => useDispatch<AppDispatch>()
-    const sidebarActive = useSelector((state: RootState) => state.sidebar.activeMenu)
     const dispacth = appDispatch()
     return (
-        <header className="flex justify-between py-4 px-3 border-b md:px-8">
-            <Link href={'/'}>
-                <div className="flex items-center">
-                    <Image src='/chatLogo.svg' alt="Site Logo" width={32} height={32}/>
-                    <h1 className="font-bold ml-2">Chat AI</h1>
-                </div>
+        <header className="flex justify-between py-4 px-3 border-b md:px-8 items-center">
+            <Link href={'/agora'}>
+                <h1 className="font-bold">AskVerse</h1>
             </Link>
             <nav>
                 <button aria-label="open menu" onClick={() => dispacth(setActiveMenu())}>
