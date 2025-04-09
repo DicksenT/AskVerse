84b8686caff65217f932088ac2d6b26afd6ab536
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 11:01:55 +0700

    Update route.ts
    add renamechat route, and return deleted chat

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -1,16 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
-import { requireAuth } from "../../../middleware/requireAuth";
-import { deleteChat } from "../../../../backend/controllers/chatControllers";
+import { requireAuth } from "../../../lib/requireAuth";
+import { deleteChat, renameChat } from "../../../../backend/controllers/chatControllers";
 import { Types } from "mongoose";
 
+//delete chat
 export async function DELETE(req: NextRequest, {params}: {params: {chatId:string}}){
     try{
         const auth = await requireAuth(req)
         if(auth instanceof NextResponse) return auth
         const userId = auth.userId
-        const {chatId} = params
+        const {chatId} = await params
         const deletedChat = await deleteChat(new Types.ObjectId(userId), new Types.ObjectId(chatId))
-        return NextResponse.json({message: 'successfully delete Chat'}, {status: 204})
+        return NextResponse.json({message: 'successfully delete Chat', data: deletedChat}, {status: 200})
+    }catch(error){
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+}
+
+//rename chat
+export async function PATCH(req: NextRequest, {params} : {params : {chatId: string}}){
+    try{
+        const auth = await requireAuth(req)
+        if(auth instanceof NextResponse)return auth
+        const {chatId} = params
+        const {newName} = await req.json()
+        const renamedChat = await renameChat(new Types.ObjectId(auth.userId), new Types.ObjectId(chatId), newName)
+        return NextResponse.json({message: 'chat successfully renamed', data: renamedChat}, {status: 200})
     }catch(error){
         return NextResponse.json({error: error.message}, {status: 400})
     }
