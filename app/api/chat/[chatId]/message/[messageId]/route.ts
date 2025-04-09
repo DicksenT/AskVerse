ac8ec9c329b8353635d9896a46b89299eb1e8bf8
Dicksen Tan <tandicksen@gmail.com>
2025-04-09 10:59:24 +0700

    Route for add respons to messages

---
+++ app/api/chat/[chatId]/message/[messageId]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { addResponseToMessage } from "../../../../../../backend/controllers/messageController";
+import { requireAuth } from "../../../../../lib/requireAuth";
+import { Types } from "mongoose";
+
+//add Response to message
+export async function PATCH(req:NextRequest, context: {params : {chatId: string,messageId: string}}){
+    try{
+        const auth = await requireAuth(req)
+        if(auth instanceof NextResponse) return auth
+        const {params} = context
+        const {chatId, messageId} = params
+        const body = await req.json()
+        const newResponse = await addResponseToMessage(new Types.ObjectId(auth.userId), new Types.ObjectId(chatId),new Types.ObjectId(messageId), body)
+        return NextResponse.json({message: 'succesfully add Response to message', data: newResponse}, {status: 201})
+    }catch(error){
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+}
