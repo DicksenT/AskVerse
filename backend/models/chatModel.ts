e6c209771c727afa11fde886ccc5f6073cb6ea33
Dicksen Tan <tandicksen@gmail.com>
2025-04-14 23:49:04 +0700

    add .pre
    to remove all relate document

--- backend/models/chatModel.ts
+++ backend/models/chatModel.ts
@@ -1,4 +1,6 @@
 import mongoose, {Schema, model, Document, Types, models} from 'mongoose'
+import Message from './messageModel'
+import User from './userModel'
 
 export interface IChat extends Document{
     _id: Types.ObjectId
@@ -10,6 +12,19 @@ const chatScheme = new Schema<IChat>({
     userId: {type: Schema.Types.ObjectId, required: true},
     name:{type: String, required:true},
     messages: [{type: Schema.Types.ObjectId , ref: 'Message'}]
+})
+
+chatScheme.pre('findOneAndDelete', async function(next){
+    try{
+        const chat = await this.model.findOne(this.getFilter())
+        if(chat){
+            await Message.deleteMany({chatId: chat._id})
+            await User.findOneAndUpdate({_id: chat.userId}, {$pull: {chats: chat._id}})
+        }
+        next()
+    }catch(error){
+        next(error)
+    }
 })
 
 const Chat = (models.Chat as mongoose.Model<IChat>) || model<IChat>("Chat", chatScheme);
--- backend/models/messageModel.ts
+++ backend/models/messageModel.ts
@@ -1,20 +1,37 @@
-import mongoose from 'mongoose'
-import {Schema, model, Document, Types, models} from 'mongoose'
+import mongoose, { Schema, model, Document, Types, models } from 'mongoose'
+import Response from './responseModel'
 
-export interface IMessage extends Document{
-    _id: Types.ObjectId
-    chatId: Types.ObjectId
-    question: string
-    response: Types.ObjectId
+export interface IMessage extends Document {
+  _id: Types.ObjectId
+  chatId: Types.ObjectId
+  question: string
+  response: Types.ObjectId[]
 }
 
 const messageScheme = new Schema<IMessage>({
-    chatId: {type: Schema.Types.ObjectId, ref: 'Chat', required: true},
-    question:{type:String, required:true},
-    response:[{type: Schema.Types.ObjectId, ref:'Response'}]
+  chatId: { type: Schema.Types.ObjectId, ref: 'Chat', required: true },
+  question: { type: String, required: true },
+  response: [{ type: Schema.Types.ObjectId, ref: 'Response' }]
 })
 
+// ✅ Delete related responses on message delete
+messageScheme.pre('deleteMany', { document: false, query: true }, async function (next) {
+  try {
+    const messages = await this.model.find(this.getFilter())
+    const msgIds = messages.map(msg => msg._id)
+    if (msgIds.length > 0) {
+      await Response.deleteMany({ msgId: { $in: msgIds } })
+    }
+    next()
+  } catch (err) {
+    next(err)
+  }
+})
 
-const Message = models.Message as mongoose.Model<IMessage> ||  model<IMessage>('Message', messageScheme)
+// ✅ Allow reload in dev
+if (mongoose.models.Message) {
+  delete (mongoose.models as any).Message
+}
 
+const Message = model<IMessage>('Message', messageScheme)
 export default Message
