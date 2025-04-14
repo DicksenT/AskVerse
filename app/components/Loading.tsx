2d10727b08e8ef8b32f72192cbba3bd9c11a97cf
Dicksen Tan <tandicksen@gmail.com>
2025-04-14 14:14:04 +0700

    Update Loading.tsx
    add spacing below loading

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Loading.css'
 export const Loading = () =>{
     return(
-        <div className="container">
+        <div className="container mb-10">
 	<div className="box"></div>
 	<div className="box"></div>
 	<div className="box"></div>
