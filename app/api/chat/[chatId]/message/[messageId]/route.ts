3e336b227c54f6a15424d5cfca935c056202d4d3
Dicksen Tan <tandicksen@gmail.com>
2025-04-16 14:50:17 +0700

    update handler params

--- app/api/chat/[chatId]/message/[messageId]/route.ts
+++ app/api/chat/[chatId]/message/[messageId]/route.ts
@@ -3,31 +3,29 @@ import { addResponseToMessage } from "../../../../../../backend/controllers/mess
 import { requireAuth } from "../../../../../lib/requireAuth";
 import { Types } from "mongoose";
 
-//add Response to message
 export async function PATCH(
-    req: NextRequest,
-    context: { params: { chatId: string; messageId: string } }
-  ) {
-    try {
-      const auth = await requireAuth(req);
-      if (auth instanceof NextResponse) return auth;
-  
-      const { chatId, messageId } = context.params;
-      const body = await req.json();
-  
-      const newResponse = await addResponseToMessage(
-        new Types.ObjectId(auth.userId),
-        new Types.ObjectId(chatId),
-        new Types.ObjectId(messageId),
-        body
-      );
-  
-      return NextResponse.json(
-        { message: "Successfully added response to message", data: newResponse },
-        { status: 201 }
-      );
-    } catch (error: any) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    }
+  req: NextRequest,
+  context: { params: Record<string, string> }
+) {
+  try {
+    const auth = await requireAuth(req);
+    if (auth instanceof NextResponse) return auth;
+
+    const { chatId, messageId } = context.params;
+    const body = await req.json();
+
+    const newResponse = await addResponseToMessage(
+      new Types.ObjectId(auth.userId),
+      new Types.ObjectId(chatId),
+      new Types.ObjectId(messageId),
+      body
+    );
+
+    return NextResponse.json(
+      { message: "Successfully added response to message", data: newResponse },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
   }
-  
+}
