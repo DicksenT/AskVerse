4b3dfe8744d5461be846863f3f069340c44f819f
Dicksen Tan <tandicksen@gmail.com>
2025-04-16 12:00:28 +0700

    Update route.ts

--- app/api/chat/[chatId]/message/[messageId]/route.ts
+++ app/api/chat/[chatId]/message/[messageId]/route.ts
@@ -4,11 +4,10 @@ import { requireAuth } from "../../../../../lib/requireAuth";
 import { Types } from "mongoose";
 
 //add Response to message
-export async function PATCH(req:NextRequest, context: {params : {chatId: string,messageId: string}}){
+export async function PATCH(req:NextRequest, {params}: {params : {chatId: string,messageId: string}}){
     try{
         const auth = await requireAuth(req)
         if(auth instanceof NextResponse) return auth
-        const {params} = context
         const {chatId, messageId} = await params
         const body = await req.json()
         const newResponse = await addResponseToMessage(new Types.ObjectId(auth.userId), new Types.ObjectId(chatId),new Types.ObjectId(messageId), body)
