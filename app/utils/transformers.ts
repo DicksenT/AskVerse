4a7d219f3951ad940a602c84e0d41bf3e11d3533
Dicksen Tan <tandicksen@gmail.com>
2025-04-16 09:11:51 +0700

    Update transformers.ts

--- app/utils/transformers.ts
+++ app/utils/transformers.ts
@@ -1,22 +1,22 @@
 import { chatStructure, messageStructure, responseStructure } from "../interfaces";
 
-export const transformMessage = (message: any): messageStructure =>({
+export const transformMessage = (message: messageStructure): messageStructure =>({
     //dont need to stringify _id since mongo auto-conver
     _id: message._id,
     question: message.question,
     //stringify the chat id since it not auto-convert
     chatId: message.chatId.toString(),
-    response: [{...message.response, isLoading: true}]
+    response: [{isLoading: true, _id: '', msgId:'', Cohere:null,Gemini:null,OpenAI:null,DeepSeek:null,Claude:null, personality:'normal'}]
 })
 
-export const transformChat = (chat: any): chatStructure =>({
+export const transformChat = (chat: chatStructure): chatStructure =>({
     _id: chat._id,
     userId: chat.userId.toString(),
     name: chat.name,
     messages: chat.messages.map((message:any) =>message.toString())
 })
 
-export const transformResponse = (response: any): responseStructure=>({
+export const transformResponse = (response: Partial<responseStructure>): responseStructure=>({
     _id: response._id,
     msgId: response.msgId.toString(),
     isLoading: false,
