af1eb1a7ad0d24c891008fa2f225d2f048712fbf
Dicksen Tan <tandicksen@gmail.com>
2025-04-16 15:47:22 +0700

    update handlers params

--- app/api/chat/[chatId]/message/route.ts
+++ app/api/chat/[chatId]/message/route.ts
@@ -17,11 +17,11 @@ export async function POST(req: NextRequest){
 }
 
 //fetch message
-export async function GET(req: NextRequest, context: {params: {chatId: string}}){
+export async function GET(req: NextRequest, {params}: {params: Promise<{chatId: string}>}){
     try{
         const auth = await requireAuth(req)
         if(auth instanceof NextResponse) return auth
-        const {chatId} =  context.params
+        const {chatId} =  await params
         const messages = await getMessages(new Types.ObjectId(auth.userId), new Types.ObjectId(chatId))
  
         return NextResponse.json({messages: 'messages successfully fetched', data: messages}, {status: 200})
