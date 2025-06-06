fe523bad9abfc8d7b62d953a7246316490301753
Dicksen Tan <tandicksen@gmail.com>
2025-06-06 12:15:50 +0700

    Sanitize markdown

--- app/components/Markdown.tsx
+++ app/components/Markdown.tsx
@@ -7,6 +7,7 @@ import remarkToc from 'remark-toc';
 import rehypeRaw from 'rehype-raw';
 import rehypeHighlight from 'rehype-highlight';
 import 'highlight.js/styles/github.css';
+import sanitizeHtml from 'sanitize-html'
 
 /** Preprocess Markdown to improve line breaks & clarity */
 function preprocessMarkdown(md: string): string {
@@ -23,14 +24,19 @@ type MarkdownRendererProps = {
 /** Renders enhanced Markdown with GFM, raw HTML, syntax highlight, TOC, line breaks */
 export default function MarkdownRenderer({ content }: MarkdownRendererProps) {
   const processed = preprocessMarkdown(content);
-
+  const cleanMarkdown = sanitizeHtml(processed, {
+  allowedTags: [ 'b', 'i', 'em', 'strong', 'a', 'code', 'pre', 'p', 'ul', 'ol', 'li' ],
+  allowedAttributes: {
+    a: ['href'],
+  },
+});
   return (
     <ReactMarkdown
       className="prose max-w-full text-text overflow-x-auto text-wrap"
       remarkPlugins={[remarkGfm, remarkBreaks, remarkToc]}
       rehypePlugins={[rehypeRaw, rehypeHighlight]}
     >
-      {processed}
+      {cleanMarkdown}
     </ReactMarkdown>
   );
 }
